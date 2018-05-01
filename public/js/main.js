fdeb586e4a543cdf0626692874cf25c9069302e8
Kevin van Huit <kevin.vanhuit@notive.nl>
2018-05-01 16:29:17 +0200

    added web3js and worked on contrats

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,61 @@
-$(document).ready(function () {
+window.addEventListener('load', function() {
 
-    var Web3 = require('web3');
-    var web3 = new Web3();
-    web3.setProvider(new web3.providers.HttpProvider());
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+        // Use Mist/MetaMask's provider
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
 
-
-    console.log(web3); // {eth: .., shh: ...} // it's here!
-
+    // Now you can start your app & access web3 freely:
+    startApp();
 });
+
+function startApp() {
+    var coinbase = web3.eth.coinbase;
+    var account = web3.eth.accounts[0];
+    var network = getNetwork();
+    var accountInterval = setInterval(function() {
+        if (web3.eth.accounts[0] !== account) {
+            account = web3.eth.accounts[0];
+            location.reload();
+        }
+    }, 100);
+
+    web3.eth.getBalance(coinbase, function(error, result){
+    if(!error)
+        console.log(web3.fromWei(result.toNumber()), 'ether');
+    else
+        console.error(error);
+    });
+
+
+
+}
+
+function getNetwork() {
+    var netId =  web3.version.network;
+    switch (netId) {
+        case "1":
+            console.log('This is mainnet');
+            break;
+        case "2":
+            console.log('This is the deprecated Morden test network.');
+            break;
+        case "3":
+            console.log('This is the ropsten test network.');
+            break;
+        case "4":
+            console.log('This is the Rinkeby test network.');
+            break;
+        case "42":
+            console.log('This is the Kovan test network.');
+            break;
+        default:
+            console.log('This is an unknown network.')
+    }
+
+    return netId;
+}
