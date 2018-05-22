67ba2a811676d1ff0c7fdeff9e34010c62b1aca1
Kevin van Huit <kevin.vanhuit@notive.nl>
2018-05-22 10:52:40 +0200

    Added backup networks

--- truffle.js
+++ truffle.js
@@ -3,16 +3,19 @@ var secret = require('./config/secret.js');
 
 module.exports = {
     networks: {
-        // rinkeby: {
-        //     host: "127.0.0.1",
-        //     port: 8545,
-        //     network_id: "4",
-        //     from: "0xf5c6acc651ef804471bd7c1f8a1fff04d7f19c9c",
-        //     gas: 4600000
-        // }
         rinkeby: {
             provider: new HDWalletProvider(secret.MNEMONIC, 'https://rinkeby.infura.io/ALZ6zu5v1wM4s4xnMU7a'),
             network_id: 4,
+            gas: 5000000
+        },
+        ropsten: {
+            provider: new HDWalletProvider(secret.MNEMONIC, 'https://ropsten.infura.io/ALZ6zu5v1wM4s4xnMU7a'),
+            network_id: 3,
+            gas: 5000000
+        },
+        kovan: {
+            provider: new HDWalletProvider(secret.MNEMONIC, 'https://kovan.infura.io/ALZ6zu5v1wM4s4xnMU7a'),
+            network_id: 42,
             gas: 5000000
         }
     }
